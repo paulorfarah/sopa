bef151017cd6c58c137e0b7e58d34e30ee93e49d
Paulo Farah <paulorfarah@gmail.com>
2021-01-03 09:04:34 -0300

    comment cpu, memory and io analysis

--- parser.go
+++ parser.go
@@ -240,7 +240,7 @@ func ParseHadoopResults() {
 	hadoopCommits := make(map[string]*commitPerf)
 
 	//commmit, prevCommit, runtime
-	metrics := []string{"runtime", "cpu", "memory", "io"}
+	metrics := []string{"runtime"]//, "cpu", "memory", "io"}
 
 	for _, metric := range metrics {
 		infile := "data/hadoop/hadoop_" + metric + ".csv"
