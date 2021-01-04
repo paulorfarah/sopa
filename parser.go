678beb6b9c31a2947e34537f15b85fcf958fee2f
Paulo Farah <paulorfarah@gmail.com>
2021-01-04 11:24:22 -0300

    fix error in writing results

--- parser.go
+++ parser.go
@@ -294,8 +294,8 @@ func ParseHadoopResults() {
 	// }
 
 	for _, c := range hadoopCommits {
-		row := []string{c.commitTime.String(), c.commit, "method name", c.prevCommitTime.String(), c.prevCommit, c.prevRuntime, 
-		    c.runtime, fmt.Sprintf("%f", c.diffRuntime),
+		row := []string{c.commitTime.String(), c.commit, "method name", c.prevCommitTime.String(), c.prevCommit, c.prevRuntime,
+			c.runtime, fmt.Sprintf("%f", c.diffRuntime),
 			c.prevCpu, c.cpu, fmt.Sprintf("%f", c.diffCpu),
 			c.prevMemory, c.memory, fmt.Sprintf("%f", c.diffMemory),
 			c.prevIo, c.io, fmt.Sprintf("%f", c.diffIo)}
@@ -310,7 +310,7 @@ func ParseHadoopResults() {
 			c.memory != "" &&
 			// c.diffMemory != float64(0) &&
 			c.prevIo != "" &&
-			c.io != "" &&
+			c.io != "" { //&&
 			// c.diffIo != float64(0) {
 			writer.Write(row)
 		}
